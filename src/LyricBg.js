525f34137b9a1b0045fdb662f80580173f05a4d5
j23saw <jeshsawant23@gmail.com>
2020-07-04 23:09:22 +0530

    pos

--- src/LyricBg.js
+++ src/LyricBg.js
@@ -29,12 +29,12 @@ class LyricBg extends Component{
             let vol = mic.getLevel();
             p.fill(100, p.constrain(vol*p.width, 0, 255), 100);
             //p.ellipse(p.width / 2, p.height / 2, p.map(vol, 0, 1, 0, p.width));
-            let i = 150;
+            let i = 200;
             let cloud = (this.state.wordCloud.length !== 0 && (this.state.wordCloud !== "Loading...") && this.state.wordCloud !=="Oops! Something Went Wrong.")?this.state.wordCloud:[]
             cloud.forEach(element => {
-                p.textSize(20)
+                p.textSize(10)
                 p.text(element, p.mouseX, i)
-                i += 20
+                i += 30
             });
             
         }
